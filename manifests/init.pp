f9e8c09cd6a1ff64eadf66f322ba46d8728d7fd1
Cristian Falcas <falcas@optymyze.com>
2015-08-31 18:04:05 +0300

    add more parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,6 +23,13 @@
 #
 # $manage_repos::                  Manage generation of yum repositories for subscribed content
 #
+# $insecure::                      Set to 1 to disable certificate validation
+#
+# $full_refresh_on_yum::           Refresh repo files with server overrides on every yum command
+#
+# $report_package_profile::        If set to zero, the client will not report the package profile to
+#                                  the subscription management service.
+#
 # === Examples
 #
 #  class { subscription_manager:
@@ -38,13 +45,19 @@
 # Copyright 2013 Gaël Chamoulaud <gchamoul@redhat.com>
 #
 class subscription_manager (
-  $version           = $subscription_manager::params::version,
-  $install_firstboot = $subscription_manager::params::install_firstboot,
-  $install_gui       = $subscription_manager::params::install_gui,
-  $hostname          = $subscription_manager::params::hostname,
-  $port              = $subscription_manager::params::port,
-  $baseurl           = $subscription_manager::params::baseurl,
-  $manage_repos      = $subscription_manager::params::manage_repos,
+  $version                = $subscription_manager::params::version,
+  $install_firstboot      = $subscription_manager::params::install_firstboot,
+  $install_gui            = $subscription_manager::params::install_gui,
+  $hostname               = $subscription_manager::params::hostname,
+  $port                   = $subscription_manager::params::port,
+  $insecure               = $subscription_manager::params::insecure,
+  $baseurl                = $subscription_manager::params::baseurl,
+  $manage_repos           = $subscription_manager::params::manage_repos,
+  $prefix                 = $subscription_manager::params::prefix,
+  $ca_cert_dir            = $subscription_manager::params::ca_cert_dir,
+  $repo_ca_cert           = $subscription_manager::params::repo_ca_cert,
+  $full_refresh_on_yum    = $subscription_manager::params::full_refresh_on_yum,
+  $report_package_profile = $subscription_manager::params::report_package_profile,
 ) inherits subscription_manager::params {
   class { '::subscription_manager::install': } ->
   class { '::subscription_manager::config': }
--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,13 @@ class subscription_manager::params {
 
   $hostname = 'subscription.rhn.redhat.com'
   $port = 443
-  
+  $insecure = 0
+  $prefix = '/subscription'
+
   $baseurl = 'https://cdn.redhat.com'
+  $ca_cert_dir = '/etc/rhsm/ca/'
+  $repo_ca_cert = '%(ca_cert_dir)sredhat-uep.pem'
   $manage_repos = 1
+  $full_refresh_on_yum = 0
+  $report_package_profile = 1
 }
