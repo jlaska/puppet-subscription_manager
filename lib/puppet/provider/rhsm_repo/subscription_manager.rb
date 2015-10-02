2162faf2a685ed2d2d571bc97adbb36d61b9c210
Belmin <me@iambelmin.com>
2015-10-02 14:48:35 -0400

    using sub-mgr output instead of content_overrides, fixes #3

--- lib/puppet/provider/rhsm_repo/subscription_manager.rb
+++ lib/puppet/provider/rhsm_repo/subscription_manager.rb
@@ -16,19 +16,14 @@ Puppet::Type.type(:rhsm_repo).provide(:subscription_manager) do
     subscription_manager('repos','--disable',@resource[:name])
   end
 
+  def self.enabled_repos
+    subscription_manager('repos').scan(/Repo ID: +([^\n]+)\n(?:[^\n]+\n)+Enabled: +1\n/)
+  end
+
   def self.instances
-    repo_file = '/var/lib/rhsm/cache/content_overrides.json'
-    repo_instances = Array.new
-    if File.exists?(repo_file)
-      repos = JSON.parse(File.open(repo_file).read)
-      repos.each do |repo|
-        if repo.has_key?('value') && repo['value'] == '1'
-          n = repo['contentLabel']
-          repo_instances.push( new( :name => n, :ensure => :present ))
-        end
-      end
+    enabled_repos.collect do |repo|
+      new(:name => repo, :ensure => :present)
     end
-    return repo_instances  
   end
 
   def self.prefetch(resources)
